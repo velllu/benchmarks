78ffb630e7e6405db35a346f4b3c6da796b138e3
velllu <91963404+velllu@users.noreply.github.com>
2025-01-05 18:55:10 +0100

    Fixed links not working in github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/main.css']
+  css: ['~/assets/main.css'],
+  app: {
+    baseURL: "/benchmarks/"
+  }
 })
